cef369900e873ff894384fa027eab3d4c00c6ec3
Assylzhan <strelokas@mail.ru>
2020-05-03 00:28:06 +0600

    day2 v.1.0

---
+++ LeetCode-May-Challenge/Day2-Jewels-and-Stones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day2_Jewels_and_Stones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (var ch in S)
+            {
+                if (dict.ContainsKey(ch)) dict[ch]++;
+                else dict.Add(ch, 1);
+            }
+
+            int result = 0;
+            foreach (var ch in J)
+            {
+                if (dict.ContainsKey(ch)) result += dict[ch];
+            }
+
+            return result;
+        }
+    }
+}
