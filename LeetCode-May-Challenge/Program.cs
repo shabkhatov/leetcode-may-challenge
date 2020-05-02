0e3e4014f22156dcfe242b85da6c84755a635895
Assylzhan <strelokas@mail.ru>
2020-05-03 00:27:57 +0600

    day1 v.1.0

---
+++ LeetCode-May-Challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode_May_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
