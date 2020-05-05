d103434cf4fa0bf7fb6991d65fe50b8194eef056
Assylzhan <strelokas@mail.ru>
2020-05-05 14:15:08 +0600

    Day 5  v.1.0

---
+++ LeetCode-May-Challenge/Day5-First-Unique-Character-In-String.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day5_First_Unique_Character_In_String
+    {
+        public int FirstUniqChar(string s)
+        {
+            HashSet<char> set = new HashSet<char>();
+            int result = -1;
+
+            for (int i = 0; i < s.Length; i++) {
+                if (!set.Contains(s[i]))
+                {
+                    bool firstUniqueIndex = true;
+                    for (int j = i + 1; j < s.Length; j++) {
+                        if (s[i] == s[j]) {
+                            firstUniqueIndex = false;
+                            set.Add(s[i]);
+                            break;
+                        }
+                    }
+                    if (firstUniqueIndex) {
+                        result = i;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -6,7 +6,7 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day4_Number_Complement().FindComplement(5);
+            var result = new Day5_First_Unique_Character_In_String().FirstUniqChar("loveleetcode");
             Console.WriteLine(result);
             Console.Read();
         }
