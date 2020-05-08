ddef95161dfe495ebc8d0c31788ddd604ecab701
Assylzhan <strelokas@mail.ru>
2020-05-09 02:33:39 +0600

    Day8 v.1.0

---
+++ LeetCode-May-Challenge/Day8-Check-Straight-Line.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day8_Check_Straight_Line
+    {
+        public bool CheckStraightLine(int[][] coordinates)
+        {
+            if (coordinates.Length == 2) return true;
+            var x1 = coordinates[0][0];
+            var y1 = coordinates[0][1];
+            var x2 = coordinates[1][0];
+            var y2 = coordinates[1][1];
+            if (x1 == x2)
+            {
+                foreach (var coordinate in coordinates)
+                {
+                    if (coordinate[0] != x1) return false;
+                }
+            }
+            else {
+                double eps = 0.0001;
+                double k = (double)(y2 - y1) / (x2 - x1);
+                foreach (var coordinate in coordinates) {
+                    if (Math.Abs(coordinate[1] - (k * (coordinate[0] - x1) + y1)) > eps) {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
+    }
+}
--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -6,9 +6,12 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day7_Cousins_In_Binary_Tree().IsCousins(new TreeNode(
-                1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(5)))
-            , 5, 4);
+            var result = new Day8_Check_Straight_Line().CheckStraightLine(new int[][] {
+                 new int []{1, 2},
+                 new int []{2, 3},
+                 new int []{3, 4},
+                 new int []{4, 5}
+            });
             Console.WriteLine(result);
             Console.Read();
         }
