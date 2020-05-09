4b645e189d593aba77a76b72500b43971f56eead
Assylzhan <strelokas@mail.ru>
2020-05-09 15:56:58 +0600

    Day 9 v.1.0

---
+++ LeetCode-May-Challenge/Day9-Valid-Perfect-Square.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day9_Valid_Perfect_Square
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            if (num <= 0) return false;
+            for (int i = 1; i * i <= num; i++) {
+                try
+                {
+                    if (checked(i * i) == num) return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -6,12 +6,7 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day8_Check_Straight_Line().CheckStraightLine(new int[][] {
-                 new int []{1, 2},
-                 new int []{2, 3},
-                 new int []{3, 4},
-                 new int []{4, 5}
-            });
+            var result = new Day9_Valid_Perfect_Square().IsPerfectSquare(16);
             Console.WriteLine(result);
             Console.Read();
         }
