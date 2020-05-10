256e7fae3c48178ae451c7d50c77cec064db5501
Assylzhan <strelokas@mail.ru>
2020-05-10 14:06:05 +0600

    Day 10 v.1.0

---
+++ LeetCode-May-Challenge/Day10-Find-the-Town-Jugde.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day10_Find_the_Town_Jugde
+    {
+        public int FindJudge(int N, int[][] trust)
+        {
+            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+            foreach (var t in trust) {
+                if (dict.ContainsKey(t[1])) dict[t[1]].Add(t[0]);
+                else dict.Add(t[1], new List<int>() { t[0] });
+            }
+            if (N == 1 && dict.Count == 0) return 1;
+
+         
+            int result = -1;
+            foreach (var d in dict)
+            {
+                if (d.Value.Count == N - 1) {
+                    if (result != -1) return -1;
+                    foreach (var dd in dict) {
+                        if (dd.Value.Contains(d.Key)) return -1;
+                    }
+                    result = d.Key;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -6,8 +6,14 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day9_Valid_Perfect_Square().IsPerfectSquare(16);
-            Console.WriteLine(result);
+            var result = new Day10_Find_the_Town_Jugde().FindJudge(4, new int[][] {
+                new int[]{ 1,3},
+                new int[]{ 1,4},
+                new int[]{ 2,3},
+                new int[]{ 2,4},
+                new int[]{ 4,3}
+            }); 
+            Console.WriteLine("Result " + result);
             Console.Read();
         }
     }
