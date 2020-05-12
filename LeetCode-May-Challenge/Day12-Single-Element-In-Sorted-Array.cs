3a093e97a33609a1102df69da9104cc8b890137e
Assylzhan <strelokas@mail.ru>
2020-05-12 13:36:44 +0600

    Day 12 v.1.0

---
+++ LeetCode-May-Challenge/Day12-Single-Element-In-Sorted-Array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day12_Single_Element_In_Sorted_Array
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i+=2) {
+                if (i == nums.Length - 1) return nums[i];
+                if (nums[i] != nums[i + 1]) return nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
