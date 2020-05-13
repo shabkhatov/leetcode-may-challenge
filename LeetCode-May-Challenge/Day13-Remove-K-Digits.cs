ba878ba92be5a2c50de9ba9822b8b469f39918da
Assylzhan <strelokas@mail.ru>
2020-05-13 16:09:41 +0600

    day 13 v.1.0

---
+++ LeetCode-May-Challenge/Day13-Remove-K-Digits.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day13_Remove_K_Digits
+    {
+        public string RemoveKdigits(string num, int k)
+        {
+            StringBuilder sb = new StringBuilder(num);
+            int i = 0;
+            if (sb.Length == k) return "0";
+            while (k > 0 && sb.Length > 0) {
+                if (sb[i] == '0') { sb.Remove(i, 1); continue; }
+                if (sb[i] <= sb[i + 1])
+                {
+                    int j = i + 1;
+                    while(j < sb.Length-1 && sb[j+1] >= sb[j])
+                    {
+                        j++;
+                    }
+                    sb.Remove(j, 1);
+                }
+                else sb.Remove(i, 1);
+                k--;
+            }
+            while (sb.Length > 0 && sb[0] == '0') sb.Remove(0, 1);
+
+            return sb.Length > 0 ? sb.ToString() : "0";
+        }
+    }
+}
--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -6,13 +6,7 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day10_Find_the_Town_Jugde().FindJudge(4, new int[][] {
-                new int[]{ 1,3},
-                new int[]{ 1,4},
-                new int[]{ 2,3},
-                new int[]{ 2,4},
-                new int[]{ 4,3}
-            }); 
+            var result = new Day13_Remove_K_Digits().RemoveKdigits("222643", 1);
             Console.WriteLine("Result " + result);
             Console.Read();
         }
