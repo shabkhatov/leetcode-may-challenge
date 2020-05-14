8c1dd96fd8933b03981748a0d0393a9b413cc7d1
Assylzhan <strelokas@mail.ru>
2020-05-14 15:09:31 +0600

    Надо было node(char){ isEndOfWord, children = new node[26]}

--- LeetCode-May-Challenge/Day14-Implement-Trie-Prefix.cs
+++ LeetCode-May-Challenge/Day14-Implement-Trie-Prefix.cs
@@ -2,41 +2,60 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace LeetCode_May_Challenge
 {
     class Day14_Implement_Trie_Prefix
     {
+        public class TreeNode<T>{
+            public ICollection<T> values { get; }
+            public TreeNode() {
+                values = new List<T>() { };
+            }
+
+            public void Insert(T item)
+            {
+                values.Add(item);
+            }
+            public bool Search(T item)
+            {
+                return values.Contains(item);
+            }
+            public bool SearchPrefix(string itemPrefix)
+            {
+                return values.Where(x => x.ToString().StartsWith(itemPrefix)).Any();
+            }
+
+        }
         public class Trie
         {
-            IDictionary<char, ICollection<string>> dict;
-            /** Initialize your data structure here. */
+            TreeNode<string>[] nodes;
             public Trie()
             {
-                dict = new Dictionary<char, ICollection<string>>();
+                nodes = new TreeNode<string>[30];
+                for (char i = 'a'; i <= 'z'; i++) {
+                    nodes[i - 'a'] = new TreeNode<string>();
+                }
             }
 
             /** Inserts a word into the trie. */
             public void Insert(string word)
             {
-                if (dict.ContainsKey(word[0]))
-                {
-                    dict[word[0]].Add(word);
-                }
-                else dict.Add(word[0], new List<string>() { word});
+                nodes[word[0] - 'a'].Insert(word);
             }
 
             /** Returns if the word is in the trie. */
             public bool Search(string word)
             {
-                return dict.ContainsKey(word[0]) ? dict[word[0]].Contains(word) : false;
+                return nodes[word[0] - 'a'].Search(word);
             }
 
             /** Returns if there is any word in the trie that starts with the given prefix. */
             public bool StartsWith(string prefix)
             {
-                return dict.ContainsKey(prefix[0]) ? dict[prefix[0]].Where(x => x.StartsWith(prefix)).Any() : false;
+                return nodes[prefix[0] - 'a'].SearchPrefix(prefix);
             }
         }
     }
