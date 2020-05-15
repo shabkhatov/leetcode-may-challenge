4b299fbc6a953551420e0087fe715b64613a2992
Assylzhan <strelokas@mail.ru>
2020-05-15 21:36:36 +0600

    day 15 v.1.0

--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -6,9 +6,14 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day13_Remove_K_Digits().RemoveKdigits("12346", 1);
+            var result = new Day15_Max_Sum_Circular_Subarray().MaxSubarraySumCircular(new int[] {
+                -9, 14, 24, -14, 12, 18, -18, -10, -10, -23, -2, -23, 11, 12, 18, -9, 9,
+                    -29,
+                12, 4, -8, 15, 11, -12, -16, -9, 19, -12, 22, 16            
+            });
             Console.WriteLine("Result " + result);
             Console.Read();
         }
     }
 }
+    
