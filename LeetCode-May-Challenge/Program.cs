6312b6cb3dc01bade865dbb71682add018acbd26
Assylzhan <strelokas@mail.ru>
2020-05-20 17:38:20 +0600

    Day 20 v.1.0

--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -8,7 +8,19 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = "";
+            var result = new Day20_Kth_Smallest_in_BST().KthSmallest(
+                new Day20_Kth_Smallest_in_BST.TreeNode(5, 
+                    new Day20_Kth_Smallest_in_BST.TreeNode(3,
+                        new Day20_Kth_Smallest_in_BST.TreeNode(2,
+                            new Day20_Kth_Smallest_in_BST.TreeNode(1),
+                            null
+                        ),
+                        new Day20_Kth_Smallest_in_BST.TreeNode(4)
+                    ),
+                    new Day20_Kth_Smallest_in_BST.TreeNode(6)
+                ),
+                3
+            );
             Console.WriteLine("Result " + result);
             Console.Read();
         }
