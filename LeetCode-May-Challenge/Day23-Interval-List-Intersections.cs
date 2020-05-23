3e28005a0394cc0f47c5432cff71118e108706ad
Assylzhan <strelokas@mail.ru>
2020-05-23 20:12:02 +0600

    Day23 v.1.0

---
+++ LeetCode-May-Challenge/Day23-Interval-List-Intersections.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day23_Interval_List_Intersections
+    {
+        
+        public int[][] IntervalIntersection(int[][] A, int[][] B)
+        {
+            if (A.Length == 0 || B.Length == 0) return new int[0][];
+            int i = 0, j = 0;
+            List<int[]> result = new List<int[]>();
+
+            for (; i < A.Length && j < B.Length;) {
+                int leftA = A[i][0], rightA = A[i][1], leftB = B[j][0], rightB = B[j][1];
+                if (leftB > leftA)
+                {
+                    if (rightA < leftB)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        if (rightA <= rightB)
+                        {
+                            result.Add(new int[2] { leftB, rightA });
+                            i++;
+                        }
+                        else
+                        {
+                            result.Add(new int[2] { leftB, rightB });
+                            j++;
+                        }
+                    }
+                }
+                else {
+                    if (rightB < leftA)
+                    {
+                        j++;
+                    }
+                    else {
+                        if (rightA > rightB)
+                        {
+                            result.Add(new int[2] { leftA, rightB });
+                            j++;
+                        }
+                        else {
+                            result.Add(new int[2] { leftA, rightA });
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            return result.Count > 0 ? result.ToArray() : null;
+        }
+    }
+}
--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -8,8 +8,20 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day22_Sort_Characters_By_Frequency().FrequencySort("vasdfaerqw");
+            var result = new Day23_Interval_List_Intersections().IntervalIntersection(
+                new int[][] {
+                    new int[] {1, 8 },
+                    new int[] {16, 20 },
+                },
+                new int[][] {
+                    new int[]{2, 11 },
+                    new int[]{12, 13 }
+                }
+            );
             Console.WriteLine("Result " + result);
+            for (int i = 0; i < result.Length; i++) {
+                Console.WriteLine($"{result[i][0]} {result[i][1]}");
+            }
             Console.Read();
         }
     }
