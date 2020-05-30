78d6325ab18d93188d1a9127d1852982d1dc6ee3
Assylzhan <strelokask@gmail.com>
2020-05-30 18:48:35 +0600

    Day30 v.1.0

---
+++ LeetCode-May-Challenge/Day30-K-Closest-Points-To-Origin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode_May_Challenge
+{
+    class Day30_K_Closest_Points_To_Origin
+    {
+        public int[][] KClosest(int[][] points, int K)
+        {
+            Dictionary<int, List<int[]>> map = new Dictionary<int, List<int[]>>();
+            for (int i = 0; i < points.Length; i++) {
+                var a = points[i][0];
+                var b = points[i][1];
+                var distance = a * a + b * b;
+                if (map.ContainsKey(distance)) map[distance].Add(new int[] { a, b });
+                else map.Add(distance, new List<int[]>() { new int[] { a, b } });
+            }
+            var result = new int[K][];
+            int temp = K;
+            foreach (var kvp in map.OrderBy(x => x.Key)) {
+                if (temp <= 0) break;
+                foreach (var val in kvp.Value) {
+                    if (temp <= 0) break;
+                    result[K - temp] = new int[] { val[0], val[1] };
+                    temp--;
+                }
+            }
+
+
+            return result;
+
+        }
+    }
+}
--- LeetCode-May-Challenge/Program.cs
+++ LeetCode-May-Challenge/Program.cs
@@ -8,9 +8,14 @@ namespace LeetCode_May_Challenge
     {
         static void Main(string[] args)
         {
-            var result = new Day29_Course_Schedule().CanFinish(2, new int[][] {
-                new int[]{ 1, 0}
-            });
+            var result = new Day30_K_Closest_Points_To_Origin().KClosest(
+                new int[][] {
+                    new int[]{ 3,3},
+                    new int[]{ 5,-1},
+                    new int[]{ -2, 4}
+                },
+                2
+            );
             Console.WriteLine("Result " + result);
             Console.Read();
         }
